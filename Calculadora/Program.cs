3240113c0fd3b6e2923e1053f73f70d1b9ccc861
Br-Moura <116111491+Br-Moura@users.noreply.github.com>
2024-07-09 14:32:20 -0300

    Calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Calculadora Simples");
+
+double n1, n2;
+int result = 0;
+
+Console.WriteLine("Insira o primeiro numero");
+n1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira o segundo numero");
+n2 = double.Parse(Console.ReadLine());
+
+Console.Clear();
+
+while (result != 5)
+{
+    Console.WriteLine("para somar digite 1");
+    Console.WriteLine("para subitrair digite  2");
+    Console.WriteLine("para multiplicar digite 3");
+    Console.WriteLine("para dividir digite 4");
+    result = int.Parse(Console.ReadLine());
+    Console.Clear();
+    if (result == 1)
+    {
+        Console.WriteLine("Soma = {0}", n1 + n2);
+        break;
+    }
+    else if (result == 2)
+    {
+        Console.WriteLine("Subitrair = {0}", n1 - n2);
+        break;
+    }
+    else if (result == 3)
+    {
+        Console.WriteLine("Multiplica = {0}", n1 * n2);
+        break;
+    }
+    else if (result == 4)
+    {
+        Console.WriteLine("Dividir = {0}", n1 / n2);
+        break;
+    }
+    else{
+        Console.WriteLine("Selecione um operador");
+    }
+}
